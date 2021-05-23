0dd1d2005d0b74a818d93f42635bf330d87e7f53
Pash10g <pash10g@gmail.com>
2021-05-23 11:19:50 +0300

    Update server.js

--- mern/server/server.js
+++ mern/server/server.js
@@ -13,7 +13,6 @@ app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
-     console.log(`Successfully connected to MongoDB.`);
 
   });
   console.log(`Server is running on port: ${port}`);
