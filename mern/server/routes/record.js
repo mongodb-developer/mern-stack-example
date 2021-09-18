d2d9b4e929634ca5ef9ef2baeba4d72b386f0311
Joel Lord <joelphy@gmail.com>
2021-09-18 08:23:07 -0400

    Clean up comments from PR #5

--- mern/server/routes/record.js
+++ mern/server/routes/record.js
@@ -5,8 +5,12 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const recordRoutes = express.Router();
 
-//This will help us connect to the database
+// This will help us connect to the database
 const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
@@ -22,8 +26,8 @@ recordRoutes.route("/record").get(function (req, res) {
 
 // This section will help you get a single record by id
 recordRoutes.route("/record/:id").get(function (req, res) {
-  let db_connect = dbo.getDb("employees");
-  let myquery = { id: req.body.id };
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
   db_connect
       .collection("records")
       .findOne(myquery, function (err, result) {
@@ -34,7 +38,7 @@ recordRoutes.route("/record/:id").get(function (req, res) {
 
 // This section will help you create a new record.
 recordRoutes.route("/record/add").post(function (req, res) {
-  let db_connect = dbo.getDb("employees");
+  let db_connect = dbo.getDb();
   let myobj = {
     person_name: req.body.person_name,
     person_position: req.body.person_position,
@@ -47,8 +51,8 @@ recordRoutes.route("/record/add").post(function (req, res) {
 
 // This section will help you update a record by id.
 recordRoutes.route("/update/:id").post(function (req, res) {
-  let db_connect = dbo.getDb("employees");
-  let myquery = { id: req.body.id };
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
   let newvalues = {
     $set: {
       person_name: req.body.person_name,
@@ -66,8 +70,8 @@ recordRoutes.route("/update/:id").post(function (req, res) {
 
 // This section will help you delete a record
 recordRoutes.route("/:id").delete((req, res) => {
-  let db_connect = dbo.getDb("employees");
-  var myquery = { id: req.body.id };
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
   db_connect.collection("records").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
