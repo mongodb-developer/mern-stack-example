6819ecf46fa57cd12314ba7f0ce9d5c5c9dd2a4e
Joel Lord <joelphy@gmail.com>
2021-09-18 10:30:29 -0400

    Fixed API responses

--- mern/server/routes/record.js
+++ mern/server/routes/record.js
@@ -37,7 +37,7 @@ recordRoutes.route("/record/:id").get(function (req, res) {
 });
 
 // This section will help you create a new record.
-recordRoutes.route("/record/add").post(function (req, res) {
+recordRoutes.route("/record/add").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myobj = {
     person_name: req.body.person_name,
@@ -46,11 +46,12 @@ recordRoutes.route("/record/add").post(function (req, res) {
   };
   db_connect.collection("records").insertOne(myobj, function (err, res) {
     if (err) throw err;
+    response.json(res);
   });
 });
 
 // This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, res) {
+recordRoutes.route("/update/:id").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
   let newvalues = {
@@ -65,16 +66,18 @@ recordRoutes.route("/update/:id").post(function (req, res) {
     .updateOne(myquery, newvalues, function (err, res) {
       if (err) throw err;
       console.log("1 document updated");
+      response.json(res);
     });
 });
 
 // This section will help you delete a record
-recordRoutes.route("/:id").delete((req, res) => {
+recordRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
   db_connect.collection("records").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
+    response.status(obj);
   });
 });
 
