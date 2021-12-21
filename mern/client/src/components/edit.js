420c9b23a7ed15c992ccec1aa2f990acc010cbfb
Joel Lord <joelphy@gmail.com>
2021-12-21 06:41:04 -0500

    Added the for attribute to the labels

--- mern/client/src/components/edit.js
+++ mern/client/src/components/edit.js
@@ -70,19 +70,21 @@ export default function Edit() {
       <h3>Update Record</h3>
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label>Person's Name: </label>
+          <label htmlFor="name">Name: </label>
           <input
             type="text"
             className="form-control"
+            id="name"
             value={form.name}
             onChange={(e) => updateForm({ name: e.target.value })}
           />
         </div>
         <div className="form-group">
-          <label>Position: </label>
+          <label htmlFor="position">Position: </label>
           <input
             type="text"
             className="form-control"
+            id="position"
             value={form.position}
             onChange={(e) => updateForm({ position: e.target.value })}
           />
@@ -92,37 +94,37 @@ export default function Edit() {
             <input
               className="form-check-input"
               type="radio"
-              name="priorityOptions"
-              id="priorityLow"
+              name="positionOptions"
+              id="positionIntern"
               value="Intern"
               checked={form.level === "Intern"}
               onChange={(e) => updateForm({ level: e.target.value })}
             />
-            <label className="form-check-label">Intern</label>
+            <label htmlFor="positionIntern" className="form-check-label">Intern</label>
           </div>
           <div className="form-check form-check-inline">
             <input
               className="form-check-input"
               type="radio"
-              name="priorityOptions"
-              id="priorityMedium"
+              name="positionOptions"
+              id="positionJunior"
               value="Junior"
               checked={form.level === "Junior"}
               onChange={(e) => updateForm({ level: e.target.value })}
             />
-            <label className="form-check-label">Junior</label>
+            <label htmlFor="positionJunior" className="form-check-label">Junior</label>
           </div>
           <div className="form-check form-check-inline">
             <input
               className="form-check-input"
               type="radio"
-              name="priorityOptions"
-              id="priorityHigh"
+              name="positionOptions"
+              id="positionSenior"
               value="Senior"
               checked={form.level === "Senior"}
               onChange={(e) => updateForm({ level: e.target.value })}
             />
-            <label className="form-check-label">Senior</label>
+            <label htmlFor="positionSenior" className="form-check-label">Senior</label>
         </div>
         </div>
         <br />
