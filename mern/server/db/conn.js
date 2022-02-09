aada6691c8f3ca90ac034c75eedc23c44ff800cd
Joel Lord <joelphy@gmail.com>
2022-02-08 21:00:28 -0500

    Fixed issue #17

--- mern/server/db/conn.js
+++ mern/server/db/conn.js
@@ -14,7 +14,7 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("myFirstDatabase");
+        _db = db.db("employees");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
