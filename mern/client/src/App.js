88ad1b68d97cc6cdbca8cf26e2a3114456663dfd
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-06-28 16:59:50 -0500

    auth route

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -8,6 +8,7 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import Auth from "./components/auth";
 
 const App = () => {
   return (
@@ -16,7 +17,7 @@ const App = () => {
       <div style={{ margin: 20 }}>
       <Routes>
 
-
+        <Route exact path="/auth" element={<Auth />} />
         {/*
         <Route exact path="/" element={<RecordList />} />
         <Route path="/edit/:id" element={<Edit />} />
