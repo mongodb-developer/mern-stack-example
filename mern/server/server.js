42fdb392c959bf463284508d9a66fdd7bc6237a6
Katie Hom <52755177+katiehom@users.noreply.github.com>
2022-11-05 23:03:08 -0600

    Update server.js
    Added forward slash in dotenv path after 'config'

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config/.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
