2bcf10285dc1bf54aa0842a94ccf20dd42521abe
Pash10g <pash10g@gmail.com>
2023-03-01 08:28:57 +0000

    Changing the delete function on web

--- mern/client/src/components/recordList.js
+++ mern/client/src/components/recordList.js
@@ -27,24 +27,24 @@ export default function RecordList() {
 
   // This method fetches the records from the database.
   useEffect(() => {
-    async function getRecords() {
-      const response = await fetch(`${BASE_URL}/record/`);
-
-      if (!response.ok) {
-        const message = `An error occured: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
-
-      const records = await response.json();
-      setRecords(records);
-    }
+    
 
     getRecords();
 
     return; 
   }, [records.length]);
+  async function getRecords() {
+    const response = await fetch(`${BASE_URL}/record/`);
 
+    if (!response.ok) {
+      const message = `An error occured: ${response.statusText}`;
+      window.alert(message);
+      return;
+    }
+
+    const records = await response.json();
+    setRecords(records);
+  }
   // This method will delete a record
   async function deleteRecord(id) {
     try {
@@ -52,14 +52,12 @@ export default function RecordList() {
       /*
         Please remove once implement mern/server/routes/record.js lines 74
       */
-     alert('Please implement the "DELETE" route on mern/server/routes/record.js lines 74')
-     return;
+      alert('Please implement the "DELETE" route on mern/server/routes/record.js lines 74, press "OK" when you done')
       await fetch(`${BASE_URL}/${id}`, {
         method: "DELETE"
       });
 
-      const newRecords = records.filter((el) => el._id !== id);
-      setRecords(newRecords);
+       await  getRecords();
     }
     catch(e)
     {
