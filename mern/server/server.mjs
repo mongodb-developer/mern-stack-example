d82ca2d3f8c8b037d80e57f1a485ca58beeb91f7
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2023-07-17 21:13:58 +0300

    fix(server): update MongoDB driver to work with ES modules and promises. Update express to fix security vulnerabilies.

--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -13,5 +13,5 @@ app.use("/record", records);
 
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
