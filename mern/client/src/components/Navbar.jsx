3d0e65738e3491ec2900c9d638a83a1856af20af
codeSTACKr <codeSTACKr@gmail.com>
2024-02-10 15:34:48 -0600

    update logo link

--- mern/client/src/components/Navbar.jsx
+++ mern/client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="flex justify-between items-center mb-6">
         <NavLink to="/">
-          <img alt="MongoDB logo" className="h-10 inline" src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+          <img alt="MongoDB logo" className="h-10 inline" src="https://raw.githubusercontent.com/mongodb-developer/mern-stack-example/603144e25ba5549159d1962601337652a7bfa253/mern/client/src/assets/mongodb.svg"></img>
         </NavLink>
 
         <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
