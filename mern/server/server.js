8f53586a729b09fd1e5580d4058f6b72c0f237de
Parshwa Shah <parshwa.surat@gmail.com>
2024-09-12 17:08:24 -0400

    Solved some bugs

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,17 +1,15 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
-import uploadExcel from "./routes/bulkrecords.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // include before other routes
 app.use("/record", records);
-app.use("/bulk", uploadExcel);
 
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
